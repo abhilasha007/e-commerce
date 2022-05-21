b537f6825b9e678673b97505fc2aa3b0a6f80e14
abhilasha007 <63452863+abhilasha007@users.noreply.github.com>
2022-05-21 20:40:26 +0530

    prod cntrl, user auth profile, auth mw

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,22 @@ import dotenv from 'dotenv';
 import color from 'colors';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
-import productRoutes from './routes/productsRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+app.use(express.json()) //allows json data in body
+
 app.get('/', (req, res) => {
   res.send('API is running');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound)
 app.use(errorHandler)
